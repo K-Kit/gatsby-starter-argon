6ab3e2926401955a54fd32811829633cd47e2166
Kyle Kitlinski <kylekitlinski@gmail.com>
2019-08-03 19:50:55 -0400

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Gatsby Starter Argon Design System',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is a gatsby-starter with creative-tim\'s Argon Design System.  1-click deploy to netlify inside!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
